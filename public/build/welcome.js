55fa6173c10cf057e3375f56a17c3813fb368339
junmi-miu <khanhttk3@fpt.edu.vn>
2022-07-09 00:27:40 +0700

    make it better again

---
+++ public/build/welcome.js
@@ -0,0 +1,22 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["welcome"],{
+
+/***/ "./assets/styles/welcome.css":
+/*!***********************************!*\
+  !*** ./assets/styles/welcome.css ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./assets/styles/welcome.css"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoid2VsY29tZS5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hc3NldHMvc3R5bGVzL3dlbGNvbWUuY3NzP2Q2ODkiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gZXh0cmFjdGVkIGJ5IG1pbmktY3NzLWV4dHJhY3QtcGx1Z2luXG5leHBvcnQge307Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
